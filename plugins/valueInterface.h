e72202b6bffb469fc83cc4f584f530d3286ecff9
Alexandre Piel <alexandre.piel@zooplus.com>
2023-09-24 19:04:00 +0200

    AudioPluginHandlerInterface

--- plugins/valueInterface.h
+++ plugins/valueInterface.h
@@ -2,7 +2,7 @@
 #define _IMPORT_VALUE_INTERFACE_H_
 
 // FIXME
-#include "../../zicHost/plugins/audioPluginHandlerInterface.h"
+#include "../../zicHost/plugins/audioPlugin.h"
 #include "../../zicHost/plugins/valueInterface.h"
 
 #endif
