7be68b0e594976b0d2a18be7bb51ea92b01ba1e9
Alexandre Piel <alexandre.piel@zooplus.com>
2023-09-26 22:11:39 +0200

    seq

--- plugins/SequencerComponent.h
+++ plugins/SequencerComponent.h
@@ -2,6 +2,8 @@
 #define _UI_COMPONENT_SEQUENCER_H_
 
 #include "../plugins/component.h"
+#include "../../zicHost/plugins/stepInterface.h"
+
 #include <string>
 
 class SequencerComponent : public Component
@@ -14,6 +16,9 @@ protected:
     Point stepPosition;
     Size stepSize;
     int stepMarginTop = 15;
+    int infoMarginRight = 10;
+
+    Step *steps;
 
     void renderStep(uint8_t index)
     {
@@ -23,6 +28,11 @@ protected:
             color.a = 150;
         }
         draw.filledRect({stepPosition.x + (index * (stepSize.w + margin)), stepPosition.y}, stepSize, color);
+        if (steps[index].enabled)
+        {
+            draw.filledRect({stepPosition.x + (index * (stepSize.w + margin)), stepPosition.y}, 
+            {stepSize.w, (int)(stepSize.h * 0.33f)}, colors.stepEnabled);
+        }
     }
 
     void render()
@@ -42,6 +52,7 @@ protected:
     {
         Color background;
         Color stepBackground;
+        Color stepEnabled;
     } colors;
 
     const int margin;
@@ -50,7 +61,8 @@ public:
     SequencerComponent(ComponentInterface::Props &props)
         : Component(props),
           colors({styles.colors.foreground,
-                  styles.colors.foreground2}),
+                  styles.colors.foreground2,
+                  styles.colors.text}),
           margin(styles.margin),
           plugin(getPlugin("Sequencer"))
     {
@@ -61,6 +73,13 @@ public:
         stepPosition = {
             position.x + margin + (int)((size.w - ((stepSize.w + margin) * selectedStep->props().stepCount)) * 0.5),
             position.y + stepMarginTop};
+
+        if (stepPosition.x > infoMarginRight)
+        {
+            infoMarginRight = stepPosition.x;
+        }
+
+        steps = (Step *)plugin.data(0);
     }
 };
 
