7f09aac72f1157f9ba418f517a48f71c75bd5c55
Alexandre Piel <alexandre.piel@zooplus.com>
2023-10-15 21:58:28 +0200

    wave

--- plugins/WaveComponent.h
+++ plugins/WaveComponent.h
@@ -4,6 +4,8 @@
 #include "../plugins/component.h"
 #include <string>
 
+#include <vector>
+
 class WaveComponent : public Component
 {
 protected:
@@ -13,13 +15,20 @@ protected:
     Point wavePosition;
     void *textureWave = NULL;
 
+    struct Data
+    {
+        float modulation;
+        float time;
+    };
+
     void render()
     {
         printf("Rendering wave\n");
         draw.filledRect(wavePosition, size, colors.background);
 
         uint64_t samplesCount = 1000;
-        float *bufferSamples = (float *)plugin.data(0);
+        float *bufferSamples = (float *)plugin.data(1);
+
         int h = waveSize.h * 0.5f;
         for (int xIndex = 0; xIndex < waveSize.w; xIndex++)
         {
@@ -30,12 +39,38 @@ protected:
             draw.line({x, y1}, {x, y2}, colors.samples);
             // draw.line({x, y1}, {x, y1}, colors.samples);
         }
+
+        std::vector<Data> *dataAmp = (std::vector<Data> *)plugin.data(2);
+        for (int i = 0; i < dataAmp->size() - 1; i++)
+        {
+            Data &data1 = dataAmp->at(i);
+            Data &data2 = dataAmp->at(i + 1);
+            draw.line({(int)(wavePosition.x + waveSize.w * data1.time),
+                       (int)(wavePosition.y + waveSize.h - waveSize.h * data1.modulation)},
+                      {(int)(wavePosition.x + waveSize.w * data2.time),
+                       (int)(wavePosition.y + waveSize.h - waveSize.h * data2.modulation)},
+                      colors.amp);
+        }
+
+        std::vector<Data> *dataFreq = (std::vector<Data> *)plugin.data(3);
+        for (int i = 0; i < dataFreq->size() - 1; i++)
+        {
+            Data &data1 = dataFreq->at(i);
+            Data &data2 = dataFreq->at(i + 1);
+            draw.line({(int)(wavePosition.x + waveSize.w * data1.time),
+                       (int)(wavePosition.y + waveSize.h - waveSize.h * data1.modulation)},
+                      {(int)(wavePosition.x + waveSize.w * data2.time),
+                       (int)(wavePosition.y + waveSize.h - waveSize.h * data2.modulation)},
+                      colors.freq);
+        }
     }
 
     struct Colors
     {
         Color background;
         Color samples;
+        Color amp;
+        Color freq;
     } colors;
 
     const int margin;
@@ -45,17 +80,19 @@ protected:
 public:
     WaveComponent(ComponentInterface::Props &props)
         : Component(props),
-          colors({styles.colors.foreground, styles.colors.textDark}),
+          colors({styles.colors.foreground, styles.colors.textDark, styles.colors.foreground2, styles.colors.foreground3}),
           margin(styles.margin),
           plugin(getPlugin("Kick23"))
     {
         waveSize = {size.w - 2 * margin, size.h - 2 * margin};
         wavePosition = {position.x + margin, position.y + margin};
+
+        colors.samples.a = 200;
     }
 
     virtual void triggerRenderer() override
     {
-        int *last = (int *)plugin.data(1);
+        int *last = (int *)plugin.data(0);
         if (*last != lastUpdateUi)
         {
             lastUpdateUi = *last + 0;
