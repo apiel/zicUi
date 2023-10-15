053c8c7c20b2fa57dafe6d7afc567202676f8494
Alexandre Piel <alexandre.piel@zooplus.com>
2023-10-15 20:47:40 +0200

    wave

--- plugins/WaveComponent.h
+++ plugins/WaveComponent.h
@@ -9,32 +9,43 @@ class WaveComponent : public Component
 protected:
     AudioPlugin &plugin;
 
-    Size textureSize;
+    Size waveSize;
+    Point wavePosition;
     void *textureWave = NULL;
+
+    ValueInterface *val1 = val(getPlugin("Kick23").getValue("ENVELOP_AMP_MOD_1"));
+    ValueInterface *val2 = val(getPlugin("Kick23").getValue("ENVELOP_AMP_MOD_2"));
+    ValueInterface *val3 = val(getPlugin("Kick23").getValue("ENVELOP_AMP_MOD_3"));
+    ValueInterface *val4 = val(getPlugin("Kick23").getValue("ENVELOP_AMP_MOD_4"));
+    ValueInterface *val5 = val(getPlugin("Kick23").getValue("ENVELOP_AMP_TIME_1"));
+    ValueInterface *val6 = val(getPlugin("Kick23").getValue("ENVELOP_AMP_TIME_2"));
+    ValueInterface *val7 = val(getPlugin("Kick23").getValue("ENVELOP_AMP_TIME_3"));
+    ValueInterface *val8 = val(getPlugin("Kick23").getValue("ENVELOP_AMP_TIME_4"));
+    ValueInterface *val9 = val(getPlugin("Kick23").getValue("ENVELOP_FREQ_MOD_1"));
+    ValueInterface *val10 = val(getPlugin("Kick23").getValue("ENVELOP_FREQ_MOD_2"));
+    ValueInterface *val11 = val(getPlugin("Kick23").getValue("ENVELOP_FREQ_MOD_3"));
+    ValueInterface *val12 = val(getPlugin("Kick23").getValue("ENVELOP_FREQ_MOD_4"));
+    ValueInterface *val13 = val(getPlugin("Kick23").getValue("ENVELOP_FREQ_TIME_1"));
+    ValueInterface *val14 = val(getPlugin("Kick23").getValue("ENVELOP_FREQ_TIME_2"));
+    ValueInterface *val15 = val(getPlugin("Kick23").getValue("ENVELOP_FREQ_TIME_3"));
+    ValueInterface *val16 = val(getPlugin("Kick23").getValue("ENVELOP_FREQ_TIME_4"));
 
     void render()
     {
-        if (textureWave == NULL)
+        draw.filledRect(wavePosition, size, colors.background);
+
+        uint64_t samplesCount = 1000;
+        float *bufferSamples = (float *)plugin.data(0);
+        int h = waveSize.h * 0.5f;
+        for (int xIndex = 0; xIndex < waveSize.w; xIndex++)
         {
-            textureWave = draw.setTextureRenderer(textureSize);
-
-            draw.filledRect({0, 0}, {textureSize.w, textureSize.h}, colors.background);
-
-            uint64_t samplesCount = 1000;
-            float *bufferSamples = (float *)plugin.data(0);
-            int h = textureSize.h;
-            for (int xIndex = 0; xIndex < textureSize.w; xIndex++)
-            {
-                int x = margin + xIndex;
-                int i = xIndex * samplesCount / textureSize.w;
-                int y1 = margin + (h - (int)(bufferSamples[i] * h));
-                int y2 = margin + (h + (int)(bufferSamples[i] * h));
-                draw.line({x, y1}, {x, y2}, colors.samples);
-            }
-
-            draw.setMainRenderer();
+            int x = wavePosition.x + xIndex;
+            int i = xIndex * samplesCount / waveSize.w;
+            int y1 = wavePosition.y + (h - (int)(bufferSamples[i] * h));
+            int y2 = wavePosition.y + (h + (int)(bufferSamples[i] * h));
+            draw.line({x, y1}, {x, y2}, colors.samples);
+            // draw.line({x, y1}, {x, y1}, colors.samples);
         }
-        draw.applyTexture(textureWave, {{position.x + margin, position.y + margin}, textureSize});
     }
 
     struct Colors
@@ -52,22 +63,23 @@ public:
           margin(styles.margin),
           plugin(getPlugin("Kick23"))
     {
-        textureSize = {size.w - 2 * margin, size.h - 2 * margin};
+        waveSize = {size.w - 2 * margin, size.h - 2 * margin};
+        wavePosition = {position.x + margin, position.y + margin};
     }
 
-    virtual void triggerRenderer() override
-    {
-        // if (lastBrowser != browser->get())
-        // {
-        //     if (textureSampleWaveform != NULL)
-        //     {
-        //         draw.destroyTexture(textureSampleWaveform);
-        //         textureSampleWaveform = NULL;
-        //     }
-        //     needRendering = true;
-        // }
-        Component::triggerRenderer();
-    }
+    // virtual void triggerRenderer() override
+    // {
+    //     // if (lastBrowser != browser->get())
+    //     // {
+    //         if (textureWave != NULL)
+    //         {
+    //             draw.destroyTexture(textureWave);
+    //             textureWave = NULL;
+    //         }
+    //         needRendering = true;
+    //     // }
+    //     Component::triggerRenderer();
+    // }
 };
 
 #endif
